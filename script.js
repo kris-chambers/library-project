a95531f376fa7873cc88ada530186dee68e0be57
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2022-12-12 09:15:16 -0500

    Initial HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(bookTitle, bookAuthor, numberOfPages, readStatus) {
+  this.bookTitle = bookTitle
+  this.bookAuthor = bookAuthor
+  this.numberOfPages = numberOfPages
+  this.readStatus = readStatus
+}
+
+function addBookToLibrary() {
+  
+}
