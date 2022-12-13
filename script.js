fa29d48271c980e87b1c71a9bd21077d99523d84
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2022-12-12 20:34:19 -0500

    Work on displayLibrary function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    "bookTitle": "The Hobbit",
+    "bookAuthor": "J.R.R. Tolkien",
+    "numberOfPages": "295",
+    "readStatus": "read",
+  },
+
+  {
+    "bookTitle": "Harry Potter",
+    "bookAuthor": "J.K. Rowling",
+    "numberOfPages": "4000",
+    "readStatus": "read",
+  }
+];
 
 function Book(bookTitle, bookAuthor, numberOfPages, readStatus) {
   this.bookTitle = bookTitle
@@ -16,6 +30,16 @@ function displayLibrary() {
   for (const book of myLibrary) {
     const library = document.querySelector('.library');
     const div = document.createElement('div');
+    div.classList.add('card');
     library.append(div);
+    const title = book.bookTitle
+    const author = book.bookAuthor
+    const pages = book.numberOfPages
+    const read = book.readStatus
+    const text = document.createTextNode(`${title} by ${author}${newLineChar}`)
+    const text2 = document.createTextNode(`${pages} pages. I have ${read} this book.`)
+    div.append(text);
+    // queryForm.appendchild(linebreak);
+    div.append(text2);
   }
 }
